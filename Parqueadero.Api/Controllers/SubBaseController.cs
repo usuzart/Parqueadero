ce04fe09f653123750aa1d7fc27a526705883676
cesarGiraldo <cesar.giraldou@opitech.com.co>
2021-12-17 09:41:27 -0500

    first commit

---
+++ Parqueadero.Api/Controllers/SubBaseController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parqueadero.Application.ApplicationService;
+using Parqueadero.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parqueadero.Api.Controllers
+{
+    public class SubBaseController<TEntity, TId> : ControllerBase
+        where TEntity : Entity<TId>, new()
+        where TId : IComparable, IComparable<TId>
+    {
+
+        private readonly ICrudService<TEntity, TId> crudService;
+
+        public SubBaseController(ICrudService<TEntity, TId> crudService)
+        {
+            this.crudService = crudService;
+        }
+
+        [HttpPut]
+        public async Task<TEntity> Post(TEntity entity) 
+        {
+            return await this.crudService.InsertAsync(entity);
+
+        }
+        [HttpPost]
+        public async Task<TEntity> Put(TEntity entity)
+        {
+            return await this.crudService.UpdateAsync(entity);
+
+        }
+
+        
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<bool> Delete(TId tid)
+        {
+            return await this.crudService.DeleteByIdAsync(tid);
+
+        }
+
+        
+        [HttpGet]
+        public async Task<IEnumerable<TEntity>> Get( )
+        {
+            return await this.crudService.FindByAllAsync();
+
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<TEntity> Get(TId id)
+        {
+            return await this.crudService.FindByIdAsync(id);
+        }
+    }
+}
---
+++ Parqueadero.Domain/Parqueadero/Puesto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parqueadero.Domain
+{
+    public class Puesto : Entity<string>
+    {
+        public string Nombre { get; set; }
+        public int Numero { get; set; }
+        public Puesto(){}
+
+        private Puesto(string nombre, int numero)
+        {           
+            this.Nombre = nombre;
+            this.Numero = numero;
+        }
+
+        public static Puesto Crear(string nombre, int numero)
+        {
+            return new Puesto(nombre, numero);
+        }
+
+    }
+}
